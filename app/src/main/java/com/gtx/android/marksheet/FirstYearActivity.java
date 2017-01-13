4bb1f333dd3b32076aa5f2348c14769622830260
skjha98 <skjha98@gmail.com>
2017-01-13 09:45:04 +0530

    Second Commit 13012017 09:44:58 Modification in FirstYearActivity" Intent and Toast Added in JAVA File

--- app/src/main/java/com/gtx/android/marksheet/FirstYearActivity.java
+++ app/src/main/java/com/gtx/android/marksheet/FirstYearActivity.java
@@ -1,7 +1,10 @@
 package com.gtx.android.marksheet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FirstYearActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class FirstYearActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_year);
+
+        TextView firstSemTextView = (TextView) findViewById(R.id.firstSem);
+        firstSemTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FirstYearActivity.this, "Opening MarkSheet for 1st Semester", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView secondSem = (TextView) findViewById(R.id.secondSem);
+        secondSem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FirstYearActivity.this, "Opening MarkSheet for 2nd Semester", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
